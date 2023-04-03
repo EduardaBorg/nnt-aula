b7aa118bcd44ca9528e64eece31d7c24fc5f1af6
EduardaBorg <116927347+EduardaBorg@users.noreply.github.com>
2023-04-03 16:20:32 -0300

    exercicio 1

--- src/main/kotlin/aula_1/exercicio1.kt
+++ src/main/kotlin/aula_1/exercicio1.kt
@@ -1,4 +1,20 @@
 package aula_1
 
 class exercicio1 {
+    // Crie um arquivo Kotlin e explique as diferenças entre ele e um arquivo Java.
+    /*  Sintaxe mais concisa: em Kotlin, a sintaxe é mais concisa, o que torna o código mais fácil de ler e escrever.
+
+        Inferência de tipo: em Kotlin, o compilador pode inferir o tipo de uma variável com base no seu valor,
+        tornando a declaração de variáveis mais simples e menos verbosa.
+
+        Null Safety: Kotlin é uma linguagem "null-safe", o que significa que o compilador tenta detectar erros de
+        referência nula em tempo de compilação. Isso é diferente de Java, onde você precisa verificar manualmente se uma 
+        variável é nula antes de usá-la, para evitar erros de referência nula em tempo de execução.
+
+        Propriedades: em Kotlin, você pode declarar propriedades em vez de métodos para obter e definir valores em uma classe
+
+        Interoperabilidade com Java: Kotlin é totalmente interoperável com Java, o que significa que você pode usar
+        código Kotlin em um projeto Java e vice-versa. Isso é útil para equipes que desejam adotar Kotlin gradualmente em um
+        projeto Java existente.
+    * */
 }
