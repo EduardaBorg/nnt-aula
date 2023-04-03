7b87e5910ef0363a4f1ce325aa2b922e1965c983
EduardaBorg <116927347+EduardaBorg@users.noreply.github.com>
2023-04-03 17:41:53 -0300

    exercicio 3

---
+++ src/main/kotlin/aula_1/exercicio3.kt
@@ -0,0 +1,28 @@
+package aula_1
+
+class exercicio3 {
+
+    fun main() {
+        //Para armazenar um número decimal, deve-se utilizar o tipo de dado Double ou Float
+        val x: Double = 3.14159
+        println("Valor de x: $x")
+
+        //Para armazenar um texto, deve-se utilizar o tipo de dado String. Já para armazenar um número inteiro,
+        //pode-se utilizar o tipo de dado Int
+        val nome: String = "Maria"
+        val idade: Int = 30
+
+        println("Nome: $nome")
+        println("Idade: $idade")
+
+        // Para armazenar um número inteiro, deve-se utilizar o tipo de dado Int.
+        // Já para armazenar um número decimal, pode-se utilizar o tipo de dado Double ou Float
+        val x: Int = 10
+        val y: Double = 3.14159
+
+        println("Valor de x: $x")
+        println("Valor de y: $y")
+
+    }
+
+}
