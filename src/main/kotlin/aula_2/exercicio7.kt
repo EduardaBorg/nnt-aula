c5b7ae673d9435f89d89362e93b985ea49c1a59f
EduardaBorg <116927347+EduardaBorg@users.noreply.github.com>
2023-04-10 20:37:44 -0300

    exercicio 7

--- src/main/kotlin/aula_2/exercicio7.kt
+++ src/main/kotlin/aula_2/exercicio7.kt
@@ -1,5 +1,48 @@
 package aula_2
 
 fun main(){
+    qs1()
+    qs2()
+    qs3()
+}
 
+fun qs1() {
+    val numeros = IntArray(10) { (0..100).random() }
+    var maior = numeros[0]
+
+    for (i in 1 until numeros.size) {
+        if (numeros[i] > maior) {
+            maior = numeros[i]
+        }
+    }
+
+    println("O maior número é: $maior")
+}
+
+fun qs2() {
+    val numeros = IntArray(10)
+    var soma = 0.0
+
+    for (i in 0 until numeros.size) {
+        print("Digite o ${i + 1}º número: ")
+        numeros[i] = readLine()!!.toInt()
+        soma += numeros[i]
+    }
+
+    val media = soma / numeros.size
+
+    println("A média dos números é: $media")
+}
+
+fun qs3() {
+    val frutas = arrayOf("abacaxi", "banana", "caju", "damasco", "uva", "laranja", "ameixa", "acerola", "abacate", "goiaba")
+    var frutasComA = ""
+
+    for (fruta in frutas) {
+        if (fruta.startsWith("a")) {
+            frutasComA += "$fruta, "
+        }
+    }
+
+    println("As frutas que começam com a letra A são: $frutasComA")
 }
